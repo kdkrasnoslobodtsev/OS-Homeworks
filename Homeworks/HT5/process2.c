95461a23fbcd32fb78b29eca1fba839d4e113c21
kkrasnoslobodtsev <90769620+lyrics0red@users.noreply.github.com>
2023-06-18 19:29:41 +0300

    Add files via upload

---
+++ Homeworks/HT5/process2.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv) {
+    int fd;
+    mkfifo(argv[1], 0666);
+    char input[256];
+    char output[256];
+    
+    for(;;) {
+        fd = open(argv[1], O_RDONLY);
+        read(fd, input, 256);
+        printf("You've got a new message:\n");
+        printf("%s\n", input);
+        close(fd);
+        
+        fd = open(argv[1], O_WRONLY);
+        printf("Write your message:\n");
+        fgets(output, 256, stdin);
+        write(fd, output, strlen(output) + 1);
+        close(fd);
+    }
+    return 0;
+}
